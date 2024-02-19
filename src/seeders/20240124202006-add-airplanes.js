9e76f5cca05a986e4295520b9a0c1869a2317c68
Faizan <siddiquifaizan2480@gmail.com>
2024-02-19 23:11:31 +0530

    First commit

---
+++ src/seeders/20240124202006-add-airplanes.js
@@ -0,0 +1,46 @@
+"use strict";
+const { Op } = require("sequelize");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Airplanes", [
+      {
+        modelNumber: "boeing360",
+        capacity: 300,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        modelNumber: "boeing240",
+        capacity: 900,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        modelNumber: "airbus500",
+        capacity: 500,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Airplanes", {});
+  },
+};
---
+++ src/services/airplane-service.js
@@ -0,0 +1,76 @@
+const { AirplaneRepository } = require("../repositories");
+const AppError = require("../utils/errors/app-error");
+const { StatusCodes } = require("http-status-codes");
+const airplaneRepository = new AirplaneRepository();
+
+async function createAirplane(data) {
+  try {
+    const airplane = await airplaneRepository.create(data);
+    return airplane;
+  } catch (error) {
+    if (error.name == "SequelizeValidationError") {
+      let explaination = [];
+      error.errors.forEach((err) => {
+        explaination.push(err.message);
+      });
+      throw new AppError(explaination, StatusCodes.BAD_REQUEST);
+    }
+    throw new AppError(
+      "Something wen wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+async function getAirplanes() {
+  try {
+    const response = await airplaneRepository.getAll();
+    return response;
+  } catch (error) {
+    throw new AppError(
+      "Something wen wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+async function getAirplane(data) {
+  try {
+    const response = await airplaneRepository.get(data);
+    return response;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The resource you requested is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Something went wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function destroyAirplane(data) {
+  try {
+    const response = await airplaneRepository.destroy(data);
+    return response;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The resource you requested is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Something went wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+module.exports = {
+  createAirplane,
+  getAirplanes,
+  getAirplane,
+  destroyAirplane,
+};
