1857f72d61d4d5e3b86e85b7f1c18946e1cffc8c
Faizan <siddiquifaizan2480@gmail.com>
2024-03-21 15:04:44 +0530

    Seat models added

---
+++ src/migrations/20240321091634-create-seat.js
@@ -0,0 +1,51 @@
+"use strict";
+const { Enums } = require("../utils/common");
+const { BUISNESS, ECONOMY, FIRST_CLASS, PREMIUM_ECONOMY } = Enums.SEAT_TYPE;
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Seats", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      airplaneId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Airplanes",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      row: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      col: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.ENUM,
+        defaultValue: ECONOMY,
+        values: [BUISNESS, ECONOMY, FIRST_CLASS, PREMIUM_ECONOMY],
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Seats");
+  },
+};
